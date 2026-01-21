7d39fb25a7f1ecb946eab96cbe6e1506746cbdc5
Victor San Kho Lin <victor@sankholin.com>
2026-01-21 17:54:53 +1100

    Bumped dependencies
    * Updated the package.json name to service name
    * Opted-out CDK cli-telemetry

--- infrastructure/toolchain/stateless-stack.ts
+++ infrastructure/toolchain/stateless-stack.ts
@@ -21,6 +21,9 @@ export class StatelessStack extends cdk.Stack {
       },
       pipelineName: 'StatelessFastqSyncManagerDeploymentPipeline',
       cdkSynthCmd: ['pnpm install --frozen-lockfile --ignore-scripts', 'pnpm cdk-stateless synth'],
+      unitAppTestConfig: {
+        command: [], // TODO https://github.com/OrcaBus/service-fastq-sync-manager/issues/37
+      },
     });
   }
 }
