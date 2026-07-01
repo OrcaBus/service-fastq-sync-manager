0790a9d06b1f12f7e17d6cbf45a12e3438edd866
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-07-01 15:54:56 +1000

    feat(lambda): Adapt get_fastq_and_remaining_requirements for context-aware checks

--- app/lambdas/get_fastq_and_remaining_requirements_py/get_fastq_and_remaining_requirements.py
+++ app/lambdas/get_fastq_and_remaining_requirements_py/get_fastq_and_remaining_requirements.py
@@ -6,6 +6,7 @@ Get the fastq list row and requirements for a given fastq id
 Inputs:
   * fastqId
   * requirements
+  * hasActiveReadSetContext (optional)
 
 Outputs:
 * fastqObj
@@ -15,13 +16,17 @@ Outputs:
 """
 
 # Standard imports
-from typing import List
+from typing import Dict, List, Optional
 
 # Orcabus imports
 from orcabus_api_tools.fastq import get_fastq
 
 # Local layer imports
-from fastq_sync_tools import check_fastq_against_requirements_list, REQUIREMENT
+from fastq_sync_tools import (
+    check_fastq_against_requirements_list,
+    ContextNotEligibleError,
+    REQUIREMENT,
+)
 
 
 def handler(event, context):
@@ -31,19 +36,24 @@ def handler(event, context):
     fastq_id: str = event.get("fastqId")
     requirements: List[REQUIREMENT] = event.get("requirements", [])
     is_unarchiving_allowed: bool = event.get("isUnarchivingAllowed", False)
+    has_active_readset_context: Optional[Dict[str, str]] = event.get("hasActiveReadSetContext", None)
 
     if not fastq_id:
         raise ValueError("fastqId is required")
 
     fastq_obj = get_fastq(fastq_id, includeS3Details=True)
 
-    satisfied_requirements, unsatisfied_requirements = (
-        check_fastq_against_requirements_list(
-            fastq_obj=fastq_obj,
-            requirements=requirements,
-            is_unarchiving_allowed=is_unarchiving_allowed
+    try:
+        satisfied_requirements, unsatisfied_requirements = (
+            check_fastq_against_requirements_list(
+                fastq_obj=fastq_obj,
+                requirements=requirements,
+                is_unarchiving_allowed=is_unarchiving_allowed,
+                has_active_readset_context=has_active_readset_context,
+            )
         )
-    )
+    except ContextNotEligibleError:
+        raise
 
     return {
         "fastqObj": fastq_obj,
