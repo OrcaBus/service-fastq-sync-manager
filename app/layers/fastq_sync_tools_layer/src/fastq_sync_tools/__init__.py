93cfb44e705c189521a60e3d49a13b706d13d519
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2026-07-30 18:16:33 +1000

    feat: add Fastq Set requirement types and export shared layer utilities
    - Add FASTQ_SET_REQUIREMENT type and has_somalier_fingerprint() to globals
    - Add check_fastq_set_against_requirements_list() to utils
    - Export new symbols from fastq_sync_tools __init__.py
    - Rename FASTQ_REQUIREMENT references for consistency across existing lambdas

--- app/layers/fastq_sync_tools_layer/src/fastq_sync_tools/__init__.py
+++ app/layers/fastq_sync_tools_layer/src/fastq_sync_tools/__init__.py
@@ -3,19 +3,21 @@
 """
 Fastq tools to be used by various lambdas as needed
 """
-from .utils.globals import REQUIREMENT
+from .utils.globals import FASTQ_REQUIREMENT, FASTQ_SET_REQUIREMENT
 from .utils.exceptions import ContextNotEligibleError
 from .utils.utils import (
     has_active_readset,
     has_active_readset_in_context,
     has_qc,
     has_fingerprint,
+    has_somalier_fingerprint,
     has_compression_metadata,
     check_fastq_job,
     check_fastq_unarchiving_job,
     run_fastq_job,
     run_fastq_unarchiving_job,
     check_fastq_against_requirements_list,
+    check_fastq_set_against_requirements_list,
     check_fastq_list_against_requirements_list,
     get_pipeline_cache_config,
     is_allowed_context,
@@ -25,7 +27,8 @@ from .utils.utils import (
 
 __all__ = [
     # Requirements enum
-    "REQUIREMENT",
+    "FASTQ_REQUIREMENT",
+    "FASTQ_SET_REQUIREMENT",
     # Exceptions
     "ContextNotEligibleError",
     # All helpers
@@ -33,12 +36,14 @@ __all__ = [
     "has_active_readset_in_context",
     "has_qc",
     "has_fingerprint",
+    "has_somalier_fingerprint",
     "has_compression_metadata",
     "check_fastq_job",
     "check_fastq_unarchiving_job",
     "run_fastq_job",
     "run_fastq_unarchiving_job",
     "check_fastq_against_requirements_list",
+    "check_fastq_set_against_requirements_list",
     "check_fastq_list_against_requirements_list",
     "get_pipeline_cache_config",
     "is_allowed_context",
